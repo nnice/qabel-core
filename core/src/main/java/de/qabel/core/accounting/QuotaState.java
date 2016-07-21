b283a7c68039120f65331229b7fa7a2745bac13b
nerminnicevic <nermin.nicevic@gmail.com>
2016-07-21 16:22:48 +0200

    added calculations of free quota and quota

--- core/src/main/java/de/qabel/core/accounting/QuotaState.java
+++ core/src/main/java/de/qabel/core/accounting/QuotaState.java
@@ -1,6 +1,20 @@
 package de.qabel.core.accounting;
 
+import org.apache.commons.io.FileUtils;
+
 class QuotaState {
     public long quota;
     public long size;
+
+    public long getQuota() {
+        return quota;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public String getQuotaDescription() {
+        return FileUtils.byteCountToDisplaySize(quota - size) + " free / " + FileUtils.byteCountToDisplaySize(quota);
+    }
 }
